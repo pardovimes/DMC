c31466e89f164637bd51fe78f9bf4f36dc8ffecb
pardtheman@gmail.com <pardtheman@gmail.com>
2018-02-14 20:10:36 +0100

    First upload

---
+++ DMC/Source/DMC/DMCGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DMCGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADMCGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADMCGameMode();
+};
+
+
+
